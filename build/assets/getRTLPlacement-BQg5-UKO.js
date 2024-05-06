85416951820f42422bd3e2d533a51ae78ea224f4
Abhishek <163126855+Abhimish03@users.noreply.github.com>
2024-05-06 08:27:01 +0530

    fix: build folder changed

---
+++ build/assets/getRTLPlacement-BQg5-UKO.js
@@ -0,0 +1,1 @@
+import{s as e}from"./DefaultLayout-CjZTBQb9.js";var s=function(t,r){switch(t){case"right":return e(r)?"left":"right";case"left":return e(r)?"right":"left";default:return t}};export{s as g};
